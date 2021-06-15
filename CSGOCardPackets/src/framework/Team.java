f6ab324f259f1667e7318cb7dcab53c465114d72
GoossensJonasPXL <jonas.goossens2@student.pxl.be>
2021-06-15 22:36:35 +0200

    Update TeamClass

--- CSGOCardPackets/src/framework/Team.java
+++ CSGOCardPackets/src/framework/Team.java
@@ -1,15 +1,22 @@
 package framework;
 
+import java.awt.*;
 import java.util.List;
 
 public class Team {
+    public String name;
     public Member CEO;
+    public Member coach;
     public String website;
     public List<Player> players;
+    public Image logo;
 
-    public Team(Member CEO, String website, List<Player> players){
+    public Team(String name, Member CEO, String website, List<Player> players, Member coach, Image logo){
+        this.name = name;
         this.CEO = CEO;
         this.website = website;
         this.players = players;
+        this.coach = coach;
+        this.logo = logo;
     }
 }
